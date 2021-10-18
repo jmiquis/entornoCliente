bf1e844d5db7f0b129b73fd3bb766eb454f7f425
Jorge <jmiquis87@gmail.com>
2021-10-18 19:55:22 +0200

    ejercicios varios

---
+++ test.js
@@ -0,0 +1,16 @@
+function ejercicio1() {
+  var mensaje="";
+  for(let i=0;i<20;i++){
+    if(i%2==0){
+      mensaje+=i.toString()+",";
+    }
+    document.getElementById("resultado").value=mensaje;
+  }
+}
+
+function ejercicio2(numero) {
+  for (let i = 1; i <= numero; i++) {
+    var mensaje="";
+    mensaje=(i/)?
+  }
+}
